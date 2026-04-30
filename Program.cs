bc4ba8930cb1dfeddba9d02774ffaf4f8504848b
ashtondscottnz <ashton.dscottnz@gmail.com>
2026-05-01 08:59:41 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ascii_animator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
