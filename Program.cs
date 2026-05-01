3524c3b9429f067903e5a2e2eb4581af3b740a40
ashtondscottnz <ashton.dscottnz@gmail.com>
2026-05-01 14:06:39 +1200

    feature: add assets, config, folder paths as constant variables

--- Program.cs
+++ Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        private const string Path_Assets = "C:\\Users\\Ashton Scott\\Visual Studio\\source\\repos\\ascii-animator\\assets";
+        private const string Path_Config = "C:\\Users\\Ashton Scott\\Visual Studio\\source\\repos\\ascii-animator\\config";
+
+        static void DisplayMenu()
+        {
+            LoadMenuSplash();
+        }
+
+        static void LoadMenuSplash()
+        {
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DisplayMenu();
         }
     }
 }
